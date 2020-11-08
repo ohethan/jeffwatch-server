1ac86b5798aa6416c7a656181feaa4c24a08db1a
ohethan <ohethan@live.unc.edu>
2020-11-08 14:15:35 -0500

    Updated placeholder api

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
+const { request, response } = require('express')
 
-app.use(express.json())
-app.use(express.static('build'))
 app.use(cors())
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },  
-  {    
-    id: 2,    
-    content: "Browser can execute only Javascript",    
-    date: "2019-05-30T18:39:34.091Z",    
-    important: false  
-  },  
-  {    
-    id: 3,    
-    content: "GET and POST are the most important methods of HTTP protocol",    
-    date: "2019-05-30T19:20:14.298Z",    
-    important: true  
-  }
-]
+app.use(express.static('build'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('<img src="https://cataas.com/cat" alt="cat">')
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
-  // if id doesnt exist, can respond with either 204 or 404
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -60,28 +46,77 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
-  
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
-    })
-  }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  res.json(note)
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      res.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then(updatedNoted => {
+      response.json(updatedNoted)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+/*
+
+  user login/signup
+
+  Chart
+    GET /api/chart
+      returns 50 highest rated movies from jeffwatch users
+
+  Movie
+    GET /api/movie/:id
+      returns avg rating, number of ratings, and recent ratings of movie with id :id
+    
+    PUT /api/movie/:id
+      add new rating
+
+  Pofile
+
+*/
