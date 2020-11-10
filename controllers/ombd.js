a4afbb4a0d7241fb534b046cc7c23b2a84561b26
ohethan <ohethan@live.unc.edu>
2020-11-10 12:15:58 -0500

    add omdb route

---
+++ controllers/ombd.js
@@ -0,0 +1,18 @@
+const config = require('../utils/config')
+const axios = require('axios')
+const omdbRouter = require('express').Router()
+
+omdbRouter.get('/search/:query', async (req, res) => {
+  const url = `http://www.omdbapi.com/?s=${req.params.query}&apikey=${config.OMDB_API_KEY}`
+  const result = await axios.get(url)
+  console.log(result.data)
+  res.json(result.data)
+})
+
+omdbRouter.get('/:id', async (req, res) => {
+  const url = `http://www.omdbapi.com/?i=${req.params.id}&apikey=${config.OMDB_API_KEY}`
+  const result = await axios.get(url)
+  res.json(result.data)
+})
+
+module.exports = omdbRouter
